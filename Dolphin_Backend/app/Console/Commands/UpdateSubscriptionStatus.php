109c6734abe73e61d0d1a7defa8cec251401a497
Mitrajsinh Jadeja <meetrajsinh04@gmail.com.com>
2025-09-15 10:19:39 +0530

    UpdateSubscriptionStatu

---
+++ Dolphin_Backend/app/Console/Commands/UpdateSubscriptionStatus.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class UpdateSubscriptionStatus extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:update-subscription-status';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update the status of expired subscriptions';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->info('Updating subscription statuses...');
+
+        $expiredSubscriptions = Subscription::where('status', 'active')
+            ->where('subscription_end', '<', Carbon::now())
+            ->get();
+
+        foreach ($expiredSubscriptions as $subscription) {
+            $subscription->status = 'expired';
+            $subscription->save();
+        }
+
+        $this->info('Done.');
+    }
+}
--- Dolphin_Backend/app/Console/Kernel.php
+++ Dolphin_Backend/app/Console/Kernel.php
@@ -3,6 +3,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use function base_path;
 
 class Kernel extends ConsoleKernel
 {
@@ -20,8 +21,10 @@ class Kernel extends ConsoleKernel
         // Clean up expired tokens daily at 2 AM
         $schedule->command('tokens:cleanup --force')->dailyAt('02:00')->description('Clean up expired OAuth tokens');
 
-    }
+        // Update subscription statuses hourly (mark active subscriptions past end as expired)
+        $schedule->command('subscriptions:update-status')->hourly()->description('Mark expired subscriptions as expired in DB');
 
+    }
     /**
      * Register the commands for the application.
      */
@@ -29,6 +32,8 @@ class Kernel extends ConsoleKernel
     {
     $this->load(__DIR__.'/Commands');
 
-       require_once  base_path('routes/console.php');
+    // Console routes should be loaded via PSR-4 namespaces or Composer autoload;
+    // avoid runtime require_once and prefer namespace imports or service providers.
     }
 }
+
